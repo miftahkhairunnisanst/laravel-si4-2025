88bb4f7d5b86f5a61892490a13489fb700ef6248
MiftahKairunnisaNasution <miftahkhairunnisanst@gmail.com>
2025-10-21 18:13:13 +0700

    Membuat Fungsi Array kedalam tabel Mahasiswa Menggunakan FOR

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -52,19 +52,15 @@
         </tr>
       </thead>
       <tbody>
-        <?php $nilai_awal = 0; ?>
-        @while ($nilai_awal < $jumlah)
-
-        <tr>
-          <td>{{ $npm [$nilai_awal] }}</td>
-          <td>{{ $nama [$nilai_awal] }}</td>
+        @for ($i = 0; $i < $jumlah; $i++)
+        <tr> 
+          <td>{{ $npm[$i]}}</td>
+          <td>{{ $nama[$i]}}</td>
           <td>Perempuan</td>
-          <td>07-09-20005</td>
-          <td>Simalungun</td>
+          <td>Kota Medan</td>
         </tr>
-
-        <?php $nilai_awal++ ?>
-        @endwhile
+            
+        @endfor
       </tbody>
     </table>
   </div>
